eae09d3e30d5bce34cacb604f8d5c41abc65a07f
campagnani <thallescampagnani@gmail.com>
2019-11-03 11:46:21 -0200

    Add debug mode

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
+#define debug
+
 #include "serverTCPIP.h"
 //#include "CRCOpen.h"
 
 int  main (void) //(int argc, char **argv)
 {
-  serverMirror();
+  TCPIP_listen_mode();
+  while (true)
+  {
+    if(TCPIP_mirror_mode()) break;
+  }
+  TCPIP_close_mode();  
+
   //startCRCOpen();
   //stopCRCOpen();
   return 0;
--- serverTCPIP.h
+++ serverTCPIP.h
@@ -8,18 +8,30 @@
 #include <string>
 
 using namespace std;
- 
-int serverMirror()
+int clientSocket;
+
+int TCPIP_listen_mode()
 {
-    // Create a socket
+    #ifdef debug
+        cerr << "----------------------------" << endl;
+        cerr << "|    TCPIP_listen_mode()   |" << endl;
+        cerr << "----------------------------" << endl << endl;
+
+        cerr << "[TCPIP]  Create a socket" << endl;
+    #endif
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1)
     {
-        cerr << "Can't create a socket! Quitting" << endl;
+        #ifdef debug
+            cerr << "[TCPIP]  Can't create a socket! Quitting" << endl;
+        #endif
         return -1;
     }
- 
-    // Bind the ip address and port to a socket
+    
+    #ifdef debug
+        cerr << "[TCPIP]  Bind the ip address and port to a socket" << endl;
+    #endif
+
     sockaddr_in hint;
     hint.sin_family = AF_INET;
     hint.sin_port = htons(54000);
@@ -27,14 +39,18 @@ int serverMirror()
  
     bind(listening, (sockaddr*)&hint, sizeof(hint));
  
-    // Tell Winsock the socket is for listening
+    #ifdef debug
+        cerr << "[TCPIP]  Tell Winsock the socket is for listening" << endl;
+    #endif
     listen(listening, SOMAXCONN);
  
-    // Wait for a connection
+    #ifdef debug
+        cerr << "[TCPIP]  Wait for a connection" << endl;
+    #endif
     sockaddr_in client;
     socklen_t clientSize = sizeof(client);
  
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+    clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
  
     char host[NI_MAXHOST];      // Client's remote name
     char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
@@ -44,44 +60,66 @@ int serverMirror()
  
     if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
     {
-        cout << host << " connected on port " << service << endl;
+        #ifdef debug
+            cout << host << "[TCPIP]  connected on port " << service << endl;
+        #endif
     }
     else
     {
         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+        #ifdef debug
+            cout << host << "[TCPIP]  connected on port " << ntohs(client.sin_port) << endl;
+        #endif
     }
- 
-    // Close listening socket
+    
+    #ifdef debug
+        cerr << "[TCPIP]  Close listening socket" << endl;
+    #endif
     close(listening);
+    return 0;
+}
+
+int TCPIP_close_mode()
+{
+    cerr << "----------------------------" << endl;
+    cerr << "|    TCPIP_close_mode()    |" << endl;
+    cerr << "----------------------------" << endl << endl;
+    // Close the socket
+    close(clientSocket);
  
-    // While loop: accept and echo message back to client
+    return 0;
+}
+
+int TCPIP_mirror_mode() //Test mode
+{
     char buf[4096];
- 
-    while (true)
+    memset(buf, 0, 4096);
+
+    #ifdef debug
+        cerr << "Wait for client to send data" << endl;
+    #endif
+    int bytesReceived = recv(clientSocket, buf, 4096, 0);
+    if (bytesReceived == -1)
     {
-        memset(buf, 0, 4096);
- 
-        // Wait for client to send data
-        int bytesReceived = recv(clientSocket, buf, 4096, 0);
-        if (bytesReceived == -1)
-        {
-            cerr << "Error in recv(). Quitting" << endl;
-            break;
-        }
- 
-        if (bytesReceived == 0)
-        {
-            cout << "Client disconnected " << endl;
-            break;
-        }
- 
-        cout << string(buf, 0, bytesReceived) << endl;
- 
-        // Echo message back to client
-        send(clientSocket, buf, bytesReceived + 1, 0);
+        #ifdef debug
+            cerr << "Error in recv(). \n Quitting..." << endl;
+        #endif
+        return 1;
     }
- 
+
+    if (bytesReceived == 0)
+    {
+        #ifdef debug
+            cout << "Client disconnected..." << endl;
+        #endif
+        return 2;
+    }
+
+    cout << string(buf, 0, bytesReceived) << endl;
+
+    // Echo message back to client
+    send(clientSocket, buf, bytesReceived + 1, 0);
+        
     // Close the socket
     close(clientSocket);
  
